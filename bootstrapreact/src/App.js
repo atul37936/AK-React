b3715d2b1bf3ac07d83e5e025b2fd759ad8d91a8
Mishra Atul <140804963+atul37936@users.noreply.github.com>
2025-02-28 02:40:23 +0530

    Add files via upload
    Integrate Bootstrap template

---
+++ bootstrapreact/src/App.js
@@ -0,0 +1,28 @@
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Sectionone from "./components/sectionone";
+import Sectiontwo from "./components/sectiontwo";
+import Sectionthree from "./components/sectionthree";
+import Footer from "./components/footer";
+function App() {
+  return (
+   <>
+      <Navbar/>
+      
+        <Header/>
+      
+       <Sectionone />
+      
+        <Sectiontwo/>
+      
+        <Sectionthree/>
+      
+        <Footer/>
+        
+       
+
+   </>
+  );
+}
+
+export default App;
